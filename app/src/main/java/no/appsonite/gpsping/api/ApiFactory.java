ccc613ec3e14accc6562b8e646437ca9ebcc6665
Sergei Belozerov <belozerow@gmail.com>
2017-09-27 10:26:27 +0500

    disable no trust certificate

--- app/src/main/java/no/appsonite/gpsping/api/ApiFactory.java
+++ app/src/main/java/no/appsonite/gpsping/api/ApiFactory.java
@@ -58,46 +58,6 @@ public class ApiFactory {
         CLIENT.setConnectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS);
         CLIENT.setWriteTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS);
         CLIENT.setReadTimeout(TIMEOUT, TimeUnit.SECONDS);
-
-
-        final TrustManager[] trustAllCerts = new TrustManager[] {
-                new X509TrustManager() {
-                    @Override
-                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                    }
-
-                    @Override
-                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                    }
-
-                    @Override
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new java.security.cert.X509Certificate[]{};
-                    }
-                }
-        };
-
-        try {
-            // Install the all-trusting trust manager
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            // Create an ssl socket factory with our all-trusting manager
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-            CLIENT.setSslSocketFactory(sslSocketFactory);
-            //builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
-            CLIENT.setHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-
     }
 
     @NonNull
