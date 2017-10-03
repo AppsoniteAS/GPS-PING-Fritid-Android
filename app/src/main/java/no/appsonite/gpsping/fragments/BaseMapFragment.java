fe2f6019d595f29af084248f22a061838db5eff3
Taras <gushultaras125@gmail.com>
2017-10-03 16:55:21 +0500

    [chg] delete MapActivity. Changed BaseMapFragment for work without MapActivity

--- app/src/main/java/no/appsonite/gpsping/fragments/BaseMapFragment.java
+++ app/src/main/java/no/appsonite/gpsping/fragments/BaseMapFragment.java
@@ -1,12 +1,19 @@
 package no.appsonite.gpsping.fragments;
 
 import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.Locale;
 
-import no.appsonite.gpsping.activities.MapActivity;
+import no.appsonite.gpsping.R;
 import no.appsonite.gpsping.viewmodel.BaseFragmentViewModel;
 
 /**
@@ -14,26 +21,47 @@ import no.appsonite.gpsping.viewmodel.BaseFragmentViewModel;
  * Company: APPGRANULA LLC
  * Date: 20.01.2016
  */
-public abstract class BaseMapFragment<B extends ViewDataBinding, M extends BaseFragmentViewModel> extends BaseBindingFragment<B, M> {
-    public GoogleMap getMap() {
-        return getMapActivity().getMap();
-    }
-
-    protected MapActivity getMapActivity() {
-        return (MapActivity) getActivity();
-    }
+public abstract class BaseMapFragment<B extends ViewDataBinding, M extends BaseFragmentViewModel> extends BaseBindingFragment<B, M> implements OnMapReadyCallback {
+    private boolean mapReady = false;
+    private GoogleMap googleMap;
+    private SupportMapFragment mapFragment;
 
     public void onMapReady() {
-        if("sv".equals(Locale.getDefault().getLanguage())){
+        if ("sv".equals(Locale.getDefault().getLanguage())) {
             getMap().setMaxZoomPreference(15);
         }
     }
 
     @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady = true;
+        this.googleMap = googleMap;
+        onMapReady();
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
-        if (getMapActivity().isMapReady()) {
+        if (isMapReady()) {
             onMapReady();
         }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        mapReady = false;
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+
+    public boolean isMapReady() {
+        return mapReady;
+    }
+
+    public GoogleMap getMap() {
+        return googleMap;
     }
 }
